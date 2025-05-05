68032494f05882d25adc435c9098e305a6da8a18
alizahid0908 <alizahid0908@proton.me>
2025-05-05 17:15:18 +0500

    Added postgres db

--- app.js
+++ app.js
@@ -1,61 +1,64 @@
 import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
-
-const messages = [
-    {
-      id: uuidv4(),
-      text: "Hi there!",
-      user: "Amando",
-      added: new Date()
-    },
-    {
-      id: uuidv4(),
-      text: "Hello World!",
-      user: "Charles",
-      added: new Date()
-    }
-  ];
   
 
-app.get('/', (req, res) => {
-    res.render('index', { messages: messages });
+app.get('/', async(req, res) => {
+    try{
+        const results = await pool.query('SELECT * FROM messages ORDER BY added DESC');
+        res.render('index', { messages: results.rows });
+    }catch (error) {
+        console.error('Error fetching messages:', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 app.get('/new', (req, res) => {
     res.render('form');
 });
 
-app.get('/details/:id', (req, res) => {
-    const message = messages.find(message => message.id === req.params.id);
-    if (message) {
-        res.render('details', { message: message });
+app.get('/details/:id', async (req, res) => {
+try {
+    const result = await pool.query('SELECT * FROM messages WHERE id = $1', [req.params.id]);
+    if (result.rows.length > 0) {
+        res.render('details', { message: result.rows[0] });
     } else {
         res.status(404).send('Message not found');
     }
+} catch (error) {
+    console.error('Error fetching message details:', error);
+    res.status(500).send('Internal Server Error');  
+}
 });
 
-app.post('/new', (req, res) => {
-    if(req.body.message === "" || req.body.user === "")
-    {
-        console.error("Please fill required fields"); 
-        res.redirect('/new'); 
+app.post('/new', async (req, res) => {
+    const {message, user} = req.body;
+    if (!message || !user) {
+        return res.status(400).send('Message and user are required');
+        return res.redirect('/new?error=Message and user are required');
     }
-    else{
-        messages.push({
-            id: uuidv4(),
-            text: req.body.message,
-            user: req.body.user,
-            added: new Date()
-        });
+    try{
+        await pool.query('INSERT INTO messages (id, message, "user", added) VALUES ($1, $2, $3, NOW())',
+            [uuidv4(), message, user]
+        );
         res.redirect('/');
+    } catch (error) {   
+        console.error('Error inserting message:', error);
+        res.redirect('/new?error=Error inserting message');
+        res.status(500).send('Internal Server Error');
     }
-
 });
 
 app.listen(3000, () => {
